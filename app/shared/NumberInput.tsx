aa3fafe06d9c903844bdbbc0d3e313e7295a85cd
An Phi <blacksteed232@outlook.com>
2024-01-06 18:24:12 -0500

    fix number input

--- app/shared/NumberInput.tsx
+++ app/shared/NumberInput.tsx
@@ -7,8 +7,11 @@ import {
 import { Unstable_NumberInput as BaseNumberInput } from "@mui/base/Unstable_NumberInput";
 import { AddCircleIcon, RemoveCircleIcon } from "./Icons";
 import { isNonNullable } from "./AssertionUtils";
-import { forwardRef } from "react";
+import { forwardRef, useCallback, useEffect, useState } from "react";
+import { parseNumber } from "./CommonUtils";
+import { toNumber } from "lodash-es";
 
+// const getValue = (val: number)
 export const NumberInput = forwardRef(function NumberInput(
   props: TextFieldProps & {
     label: string;
@@ -33,47 +36,53 @@ export const NumberInput = forwardRef(function NumberInput(
     setValue,
     // ...otherProps
   } = props;
-  const _value = isNonNullable(value) ? value / (factor ?? 1) : undefined;
-  const _setValue = (val: number) => {
-    const _min = min ?? 0;
-    const _max = max ?? Number.MAX_SAFE_INTEGER;
-    const newValue = Math.max(_min, Math.min(_max, val)) * (factor ?? 1);
-    // NOTE: trick to avoid floating point error in JS
-    // See https://stackoverflow.com/questions/50778431/why-does-0-1-0-2-return-unpredictable-float-results-in-javascript-while-0-2
-    // See https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
-    setValue((step ?? 1) % 1 !== 0 ? Math.round(newValue * 10) / 10 : newValue);
-  };
+  // const _value = isNonNullable(value) ? value / (factor ?? 1) : undefined;
+  const [inputValue, setInputValue] = useState<string | number | undefined>(
+    isNonNullable(value) ? value / (factor ?? 1) : undefined
+  );
+  const _setValue = useCallback(
+    (val: number) => {
+      const _min = min ?? 0;
+      const _max = max ?? Number.MAX_SAFE_INTEGER;
+      const newValue = Math.max(_min, Math.min(_max, val)) * (factor ?? 1);
+      // NOTE: trick to avoid floating point error in JS
+      // See https://stackoverflow.com/questions/50778431/why-does-0-1-0-2-return-unpredictable-float-results-in-javascript-while-0-2
+      // See https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+      setValue(
+        (step ?? 1) % 1 !== 0 ? Math.round(newValue * 10) / 10 : newValue
+      );
+    },
+    [setValue, factor, min, max, step]
+  );
+  useEffect(() => {
+    const numericValue = toNumber(inputValue);
+    if (isNaN(numericValue)) {
+      setValue(0);
+    } else {
+      _setValue(numericValue);
+    }
+  }, [inputValue, setValue, _setValue]);
 
   return (
     <div className="flex">
-      <BaseNumberInput
-        value={_value as number}
-        onChange={(event, value) => {
-          _setValue(value ?? 0);
-        }}
-        slots={{
-          input: forwardRef(function NumberInputInput(_props, _ref) {
-            return (
-              <TextField
-                ref={_ref}
-                {..._props}
-                variant="outlined"
-                label={label}
-                InputProps={
-                  unit
-                    ? {
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            {unit}
-                          </InputAdornment>
-                        ),
-                      }
-                    : {}
-                }
-              />
-            );
-          }),
-        }}
+      <TextField
+        inputMode="numeric"
+        variant="outlined"
+        className="w-full"
+        label={label}
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        InputProps={
+          unit
+            ? {
+                startAdornment: (
+                  <InputAdornment position="start">{unit}</InputAdornment>
+                ),
+              }
+            : {}
+        }
+        // probably better to use mui's NumberInput, but it currently does not support demical
+        // See https://github.com/mui/material-ui/issues/38518
       />
       <div className="flex justify-center items-center ml-2">
         <IconButton
