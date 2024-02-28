b6d997944e9756ca16976486b5047f4288f8d942
An Phi <blacksteed232@outlook.com>
2024-02-28 06:14:05 -0500

    minor update

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  // NOTE: We're stuck with older version of material-ui since it does not support ESM and newer version
+  // contains changes that could break the app while running
+  // The recommended workaround is to set serverModuleFormat: 'cjs', but it would cause problems with other
+  // ESM modules we use
+  // See https://github.com/mui/material-ui/issues/30671
   // See https://remix.run/docs/en/main/guides/gotchas#importing-esm-packages
-  serverDependenciesToBundle: [/@mui\//, /date-fns/],
+  // See https://github.com/mui/material-ui/blob/master/examples/material-ui-remix-ts/remix.config.js
+  serverDependenciesToBundle: [/@mui\//],
 };
